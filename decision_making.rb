70eb0acd4a352c84c49c886e1a8742c70d82c458
Jasmine Hamou <jasmine.mahabadi@gmail.com>
2022-10-16 13:32:56 -0700

    Answer questions 1-7

--- decision_making.rb
+++ decision_making.rb
@@ -23,21 +23,34 @@ puts "2. Point it out to the bear?"
 puts "3. Make a dash for the next room?"
 
 if bear_choice == 1
-  "You offer the bear your " + bear_clothing + " and the bear shows you a secret passage out!"
+  puts "You offer the bear your " + bear_clothing + " and the bear shows you a secret passage out!"
 elsif bear_choice == 2
-  "You tell the bear the " + bear_clothing + " is too small and it starts to cry!"
+  puts "You tell the bear the " + bear_clothing + " is too small and it starts to cry!"
 elsif bear_choice == 3
-  "You run as fast as you can into the next room. It's full of snakes!"
+  puts "You run as fast as you can into the next room. It's full of snakes!"
 else
-  "You stay with the bear and become it's best friend!"
+  puts "You stay with the bear and become it's best friend!"
 end
 
 
 # Questions
 # 1. In English, using technical vocabulary, describe what is happening between lines 12 and 16.
+# This line of code assigns the value for the variable #bear_clothing based on the value of the variable #door_choice.
+
 # 2. What variable has a new value assigned to it after the first if statement executes?
+# bear_clothing
+
 # 3. If you changed the variable door_choice to equal 3, what would the bear_clothing value be?
+# bear_clothing = "scarf"
+
 # 4. In English, using technical vocabulary, describe what is happening between lines lines 25 and 33.
-# 5. If you changed the variable bear_choice to equal 3, what will be the final outcome be?
-# 6. If you changed the variable door_choice to equal 1, and the variable bear_choice to equal 2, what will be the final outcome be?
+# This line of code assigns the output message based on the value of variable #bear_choice.
+
+# 5. If you changed the variable bear_choice to equal 3, what will the final outcome be?
+# The final output would be, "You run as fast as you can into the next room. It's full of snakes!"
+
+# 6. If you changed the variable door_choice to equal 1, and the variable bear_choice to equal 2, what will the final outcome be?
+# The final outcome would be, "You tell the bear the hat is too small and it starts to cry!""
+
 # 7. What is your favorite ending?
+# My favorite ending is the final ending where you and the bear become best friends.
