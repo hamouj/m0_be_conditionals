e62afd03f8b18b62ba1664ef1b61f1a7335c3c88
Jasmine Hamou <jasmine.mahabadi@gmail.com>
2022-10-16 13:12:19 -0700

    Complete section 3 you do's

--- comparisons.rb
+++ comparisons.rb
@@ -99,15 +99,16 @@ age = 1
 
 # YOU DO:
 # Determine if the dog loves to play and loves treats
-
+puts loves_to_play && loves_treats
 
 # Determine if the dog loves to play and loves the dog park
-
+puts loves_to_play && loves_dog_park
 
 # Determine if the dog loves to play or loves the dog park
-
+puts loves_to_play || loves_dog_park
 
 # Determine if the dog loves to play and is a puppy
+puts loves_to_play && age < 1
 
 # What did your final line of code evaluate to? Why do you think that is? Explain.
-# ANSWER:
+# ANSWER: My final line of code evaluated to false because I set my definition of puppy as being less than 1 year old.
