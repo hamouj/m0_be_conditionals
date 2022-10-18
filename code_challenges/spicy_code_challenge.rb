ebf23326a112db49d3d2d8ba282f4d5cfba56cdb
Jasmine Hamou <jasmine.mahabadi@gmail.com>
2022-10-17 20:13:40 -0700

    Shorten code to exclude == true

--- code_challenges/spicy_code_challenge.rb
+++ code_challenges/spicy_code_challenge.rb
@@ -1,9 +1,9 @@
-good_driving_record = false
-is_over_25 = false
+good_driving_record = true
+is_over_25 = true
 
-if good_driving_record == true && is_over_25 == true
+if good_driving_record && is_over_25 
     puts "Congratulations! You get a discount on your car rental."
-elsif good_driving_record == true || is_over_25 == true
+elsif good_driving_record  || is_over_25 
     puts "You can get a car, but you need to pay full price."    
 else
     puts "You can get a car, but you need to have someone else sign for you."
